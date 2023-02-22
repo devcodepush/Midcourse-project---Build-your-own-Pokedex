1b41c233db8f29db8d3226211195bb4ddf78d808
Arman Partovifar <{armanpartovi}@users.noreply.github.com>
2023-02-22 15:54:43 +0100

    added first commits for the exercise

---
+++ main.js
@@ -0,0 +1,79 @@
+//alert for pokemon Balbasaur
+let button = document.getElementById("balbasaur");
+
+function buttonAlert() {
+  alert(
+    "It's got immunity to the poison status, it is solid in the gyms, and its attacks are solid"
+  );
+}
+button.addEventListener("click", buttonAlert);
+
+//alert for pokemon Charmander
+let button2 = document.getElementById("charmander");
+
+function buttonAlert2() {
+  alert(
+    "Charmander is known for the flame on its tail indicating life force; if it is healthy, the flame burns brightly."
+  );
+}
+button2.addEventListener("click", buttonAlert2);
+
+//alert for pokemon Caterpie
+let button3 = document.getElementById("caterpie");
+
+function buttonAlert3() {
+  alert(
+    "Caterpie is known for its voracious appetite. It will often eat leaves twice as big as its own body and after its appetite is sated it will shed its skin. It molts several times before it evolves to its next phase."
+  );
+}
+button3.addEventListener("click", buttonAlert3);
+
+//alert for pokemom Kakuna
+let button4 = document.getElementById("kakuna");
+
+function buttonAlert4() {
+  alert(
+    "Kakuna is a Bug/Poison type Pokémon introduced in Generation 1 . It is known as the Cocoon Pokémon"
+  );
+}
+button4.addEventListener("click", buttonAlert);
+
+//alert button for Ratata
+let button5 = document.getElementById("ratata");
+
+function buttonAlert5() {
+  alert(
+    "Bites anything when it attacks. Small and very quick, it is a common sight in many places."
+  );
+}
+button5.addEventListener("click", buttonAlert5);
+
+//alert button for Pikachu
+let button6 = document.getElementById("pikachu");
+
+function buttonAlert6() {
+  alert(
+    "Pikachu is widely considered to be the most popular and well-known Pokémon species, largely due to its appearance in the Pokémon anime television series as the companion of protagonist Ash Ketchum"
+  );
+}
+button6.addEventListener("click", buttonAlert6);
+
+//alert button for Vulpix
+let button7 = document.getElementById("vulpix");
+
+function buttonAlert7() {
+  alert(
+    "Vulpix is known to feign injury to escape from opponents too powerful for it to defeat."
+  );
+}
+button7.addEventListener("click", buttonAlert7);
+
+//alert button for Jigglypuff
+let button8 = document.getElementById("jigglypuff");
+
+function buttonAlert8() {
+  alert(
+    "Jigglypuff are characterized by putting their enemies to sleep by singing a lullaby. Before beginning to sing, they mesmerize the opponent with their soft, glowing eyes and, if they inflate themselves, they can sing for longer periods of time."
+  );
+}
+button8.addEventListener("click", buttonAlert8);
