25dd5a032d241de86005825c4f9063bcca5d579e
Arman Partovifar <{armanpartovi}@users.noreply.github.com>
2023-02-24 17:59:32 +0100

    remove all data in body and replace by trying in js file

--- main.js
+++ main.js
@@ -1,79 +1,84 @@
-//alert for pokemon Balbasaur
-let button = document.getElementById("balbasaur");
+//Data of all Pokemons on one array
+const arrayOfPokemons = [
+  {
+    thumbnail: "media/001.png",
+    id: 0,
+    name: "balbasaur",
+    type: ["Plant", "Poison"],
+  },
+  {
+    thumbnail: "media/002.png",
+    id: 1,
+    name: "charmander",
+    type: ["Charmander", "Fire"],
+  },
+  {
+    thumbnail: "media/003.png",
+    id: 2,
+    name: "caterpie",
+    type: ["Caterpie", "Bug"],
+  },
+  {
+    thumbnail: "media/004.png",
+    id: 3,
+    name: "kakuna",
+    type: ["Bug", "Poison"],
+  },
+  {
+    thumbnail: "media/005.png",
+    id: 4,
+    name: "ratata",
+    type: ["Ratata", "Normal"],
+  },
+  {
+    thumbnail: "media/006.png",
+    id: 5,
+    name: "pikachu",
+    type: ["Pikachu", "Electrical"],
+  },
+  {
+    thumbnail: "media/007.png",
+    id: 6,
+    name: "vulpix",
+    type: ["Vulpix", "Fire"],
+  },
+  {
+    thumbnail: "media/008.png",
+    id: 7,
+    name: "jigglypuff",
+    type: ["Normal", "Fairy"],
+  },
+];
 
-function buttonAlert() {
-  alert(
-    "It's got immunity to the poison status, it is solid in the gyms, and its attacks are solid"
-  );
-}
-button.addEventListener("click", buttonAlert);
+console.log(arrayOfPokemons);
 
-//alert for pokemon Charmander
-let button2 = document.getElementById("charmander");
+// Writing for the first pokemon all the elemnts by order: first div then button, img, p, h3, h4, h4
+const DOMobject = document.createElement("div");
+document.body.appendChild(DOMobject);
+DOMobject.id = "container1";
 
-function buttonAlert2() {
-  alert(
-    "Charmander is known for the flame on its tail indicating life force; if it is healthy, the flame burns brightly."
-  );
-}
-button2.addEventListener("click", buttonAlert2);
+const DOMbtn = document.createElement("button");
+document.getElementById("container1").appendChild(DOMbtn);
+DOMbtn.id = "balbasaurbtn";
 
-//alert for pokemon Caterpie
-let button3 = document.getElementById("caterpie");
+const DOMimg = document.createElement("img");
+document.getElementById("balbasaurbtn").appendChild(DOMimg);
+DOMimg.id = "balbasaur-image";
+DOMimg.src = "media/001.png";
 
-function buttonAlert3() {
-  alert(
-    "Caterpie is known for its voracious appetite. It will often eat leaves twice as big as its own body and after its appetite is sated it will shed its skin. It molts several times before it evolves to its next phase."
-  );
-}
-button3.addEventListener("click", buttonAlert3);
+const DOMp = document.createElement("p");
+document.getElementById("container1").appendChild(DOMp);
+DOMp.id = "number";
+DOMp.innerHTML = "N°001";
 
-//alert for pokemom Kakuna
-let button4 = document.getElementById("kakuna");
+const DOMh3 = document.createElement("h3");
+document.getElementById("container1").appendChild(DOMh3);
+DOMh3.innerHTML = "Bulbasaur";
 
-function buttonAlert4() {
-  alert(
-    "Kakuna is a Bug Poison type Pokemon introduced in Generation one . It is known as the Cocoon Pokemon"
-  );
-}
-button4.addEventListener("click", buttonAlert4);
+const DOMh4 = document.createElement("h4");
+document.getElementById("container1").appendChild(DOMh4);
+DOMh4.innerHTML = "Plant";
 
-//alert button for Ratata
-let button5 = document.getElementById("ratata");
-
-function buttonAlert5() {
-  alert(
-    "Bites anything when it attacks. Small and very quick, it is a common sight in many places."
-  );
-}
-button5.addEventListener("click", buttonAlert5);
-
-//alert button for Pikachu
-let button6 = document.getElementById("pikachu");
-
-function buttonAlert6() {
-  alert(
-    "Pikachu is widely considered to be the most popular and well-known Pokémon species, largely due to its appearance in the Pokémon anime television series as the companion of protagonist Ash Ketchum"
-  );
-}
-button6.addEventListener("click", buttonAlert6);
-
-//alert button for Vulpix
-let button7 = document.getElementById("vulpix");
-
-function buttonAlert7() {
-  alert(
-    "Vulpix is known to feign injury to escape from opponents too powerful for it to defeat."
-  );
-}
-button7.addEventListener("click", buttonAlert7);
-
-//alert button for Jigglypuff
-let button8 = document.getElementById("jigglypuff");
-
-function buttonAlert8() {
-  alert(
-    "Jigglypuff are characterized by putting their enemies to sleep by singing a lullaby. Before beginning to sing, they mesmerize the opponent with their soft, glowing eyes and, if they inflate themselves, they can sing for longer periods of time."
-  );
-}
-button8.addEventListener("click", buttonAlert8);
+const DOMotherh4 = document.createElement("h4");
+document.getElementById("container1").appendChild(DOMotherh4);
+DOMotherh4.innerHTML = "Poison";
