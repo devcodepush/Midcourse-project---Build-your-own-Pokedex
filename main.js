be1f8cd7daf2144acc5311097301ac859f5507f1
Arman Partovifar <{armanpartovi}@users.noreply.github.com>
2023-03-01 17:40:16 +0100

    added new commits

--- main.js
+++ main.js
@@ -12,49 +12,49 @@ const arrayOfPokemons = [
     id: "N°002",
     name: "CHARMANDER",
     type: ["Charmander", "Fire"],
-    clickMessage: "Bulbasaur is effective against water and rock-type Pokemon",
+    clickMessage: "Charmander is known for the flame on its tail indicating life force; if it is healthy, the flame burns brightly.",
   },
   {
     thumbnail: "media/003.png",
     id: "N°003",
     name: "CATERPIE",
     type: ["Caterpie", "Bug"],
-    clickMessage: "",
+    clickMessage: "Caterpie is known for its voracious appetite. It will often eat leaves twice as big as its own body and after its appetite is sated it will shed its skin. It molts several times before it evolves to its next phase.",
   },
   {
     thumbnail: "media/004.png",
     id: "N°004",
     name: "KAKUNA",
     type: ["Bug", "Poison"],
-    clickMessage: "",
+    clickMessage: "Kakuna is a Bug/Poison type Pokémon introduced in Generation 1 . It is known as the Cocoon Pokémon",
   },
   {
     thumbnail: "media/005.png",
     id: "N°005",
     name: "RATATA",
     type: ["Ratata", "Normal"],
-    clickMessage: "",
+    clickMessage: "Bites anything when it attacks. Small and very quick, it is a common sight in many places.",
   },
   {
     thumbnail: "media/006.png",
     id: "N°006",
     name: "PIKACHU",
     type: ["Pikachu", "Electrical"],
-    clickMessage: "",
+    clickMessage: "Pikachu is widely considered to be the most popular and well-known Pokémon species, largely due to its appearance in the Pokémon anime television series as the companion of protagonist Ash Ketchum",
   },
   {
     thumbnail: "media/007.png",
     id: "N°007",
     name: "VULPIX",
     type: ["Vulpix", "Fire"],
-    clickMessage: "",
+    clickMessage: "Vulpix is known to feign injury to escape from opponents too powerful for it to defeat.",
   },
   {
     thumbnail: "media/008.png",
     id: "N°008",
     name: "JIGGLYPUFF",
     type: ["Normal", "Fairy"],
-    clickMessage: "",
+    clickMessage: "Jigglypuff are characterized by putting their enemies to sleep by singing a lullaby. Before beginning to sing, they mesmerize the opponent with their soft, glowing eyes and, if they inflate themselves, they can sing for longer periods of time.",
   },
 ];
 
@@ -74,6 +74,7 @@ for (let index = 0; index < arrayOfPokemons.length; index++) {
 
   const imageBtn = document.createElement("button");
   imageBtn.className = "imageBtn-classname";
+
   const image = document.createElement("img");
   image.className = "image-classname";
   image.src = arrayOfPokemons[index].thumbnail;
@@ -105,11 +106,3 @@ for (let index = 0; index < arrayOfPokemons.length; index++) {
   imageBtn.appendChild(image);
 }
 
-for (let index = 0; index < arrayOfPokemons.length; index++) {
-  const alertBtn = document.getElementsByClassName("imageBtn");
-  const alertMessage = arrayOfPokemons[index].clickMessage;
-  function alertFunction() {
-    alert(alertMessage);
-  }
-  alertBtn.addEventListener("click", alertMessage);
-}
